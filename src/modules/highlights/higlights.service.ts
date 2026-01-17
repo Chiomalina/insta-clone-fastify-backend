c7adabe4bcdd94687a7f962bbafac8cbe9b4f8b0
iamchioma <chiomalinaanaso@gmail.com>
2026-01-17 06:31:39 +0100

    feat(highlights): implement highlighsService for getAll() and getById()

---
+++ src/modules/highlights/higlights.service.ts
@@ -0,0 +1,20 @@
+import type { FastifyInstance } from "fastify"
+import type { Highlight } from "./highlights.types"
+
+export const highlightsService = (fastify: FastifyInstance) => {
+    return {
+        // Used by GET /highlights
+        // In tests: fastify.transactions.highlighs.getAll() is mocked to return an arra
+        async getAll(): Promise<Highlight[]> {
+            fastify.log.info("Fetching all highlights")
+            return fastify.transactions.highlights.getAll()
+        },
+
+        // Used by GET /highlights/:id
+        // In tests: fastify.transactions.highlighs.getById(id) is mocked to return a highlight or undefined
+        async getByAll(id: number): Promise<Highlight | undefined> {
+            fastify.log.info("Fetching all highlight by id")
+            return fastify.transactions.highlights.getById(id)
+        },
+    }
+}
