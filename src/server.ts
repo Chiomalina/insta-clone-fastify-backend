9d5863fd4944629464f773f1be55aee91be1bada
iamchioma <chiomalinaanaso@gmail.com>
2026-01-17 04:42:05 +0100

    feat(tagged): register taggedRoute in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify"
 import { databasePlugin } from "./core/database/database.plugin"
 import { postsRoutes } from "./modules/posts/posts.routes"
 import { reelsRoutes } from "./modules/reels/reels.routes"
+import { taggedRoutes } from "./modules/tagged/tagged.routes"
 
 const fastify = Fastify({
     logger: true,
@@ -12,6 +13,12 @@ fastify.register(databasePlugin)
 
 // Register our new posts routes
 fastify.register(postsRoutes)
+
+// Register our new reels routes
+fastify.register(reelsRoutes)
+
+// Register our new tagged routes
+fastify.register(taggedRoutes)
 
 // Register our new reels routes
 fastify.register(reelsRoutes)
