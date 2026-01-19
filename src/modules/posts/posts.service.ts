49a884c553b0dbdc18c235c0dd8ca7f4d1e5a656
iamchioma <chiomalinaanaso@gmail.com>
2026-01-19 04:05:36 +0100

    feat(posts): modify create method to accept an optional file buffer and filename and the use the fileStorageService

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,13 +1,38 @@
 import type { FastifyInstance } from "fastify"
 import type { CreatePostDto, Post } from "./posts.types"
+import { fileStorageService } from "../../common/file-storage.service"
+
+type CreatePostData = {
+    img_url: string // URL of the uploaded image coming from storage service
+    caption: string
+}
+
+type CreatePostServiceArgs = {
+    caption: string
+    imageFile: { buffer: Buffer; filename: string }
+}
 
 const postsService = (fastify: FastifyInstance) => {
     return {
-        create: async (postData: CreatePostDto) => {
+        create: async (data: CreatePostServiceArgs) => {
             fastify.log.info(`Creating a new post`)
+
+            let img_url = data.caption // Fallback if no image, or placeholder
+
+            if (data.imageFile) {
+                // If an image is provided, save it and get the img_url
+                img_url = await fileStorageService.saveImage(
+                    data.imageFile.buffer,
+                    data.imageFile.filename
+                )
+            }
+
             // This will use the MOCK `transactions` in our test,
             // and the REAL `transactions` in our live application.
-            const post = fastify.transactions.posts.create(postData)
+            const post = fastify.transactions.posts.create({
+                img_url,
+                caption: data.caption,
+            })
             return post
         },
 
